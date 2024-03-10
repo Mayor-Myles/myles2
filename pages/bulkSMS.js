a842c6c10cbb3827a98012ed21b0d6a786193cff
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-10 07:32:47 +0100

    Update bulkSMS.js

--- pages/bulkSMS.js
+++ pages/bulkSMS.js
@@ -11,7 +11,7 @@ import {
   Button,
   ChakraProvider,
 Flex,
-  Text
+  Text,Center,Spinner
 } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
@@ -19,7 +19,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import NavbarBottom from "../components/navbarBottom";
 import Header from '../components/header';
 import $ from 'jquery';
-
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { pageLoading,thisPage,mode } from "../components/recoil";
 
 const SendBulkSMS = () => {
   
@@ -28,7 +29,35 @@ const[cost,setCost] =  useState(0);
   const[numbers,setNumbers] = useState("");
 
   const[message,setMessage] = useState("");
+const currentMode = useRecoilValue(mode);
+  const setMode = useSetRecoilState(mode);
+  const [spin, setSpin] = useState(true);
+  const [selected, setSelected] = useState(null);
+  const [network, setNetwork] = useState('mtn');
+const loadingPage = useRecoilValue(pageLoading);
+const setLoadingPage = useSetRecoilState(pageLoading);
+  const currentPage = useRecoilValue(thisPage);
+const setCurrentPage = useSetRecoilState(thisPage);
 
+
+
+  
+  
+useEffect(() => {
+  
+  const userChoice = window.localStorage.getItem("mode");
+
+  if(userChoice === "dark"){
+  setMode("dark");
+  }
+  else{
+    setMode("light");
+  }
+
+  setCurrentPage("bulkSMS");
+  setLoadingPage(false);
+
+},[]);
 
   useEffect(()  => {
 
@@ -45,47 +74,20 @@ const[cost,setCost] =  useState(0);
   return (
     <>
       <Header/>
-    <ChakraProvider>
+   <ChakraProvider>
+     {loadingPage ?(
+<Center bg={currentMode ==="dark" && "black"} mt="" height="100vh">
+            <Box
+              position="absolute"
+              top="40%"
+             bg={currentMode==="dark" && "black"}
+              p={4}
+              maxW="md"
+              borderWidth=",0px"
+        borderColor="#657ce0"
+              borderRadius="md"
+              boxShadow="lg"
+              textAlign="center"
 
-    <Container  maxW="xl" centerContent p={4}>
-      <Box m="auto" textAlign="center">
-        <Heading as="h1" mb={6}>
-          Send Bulk SMS
-        </Heading>
-      </Box>
-
-      <Box width={{ base: '100%', md: '50%' }} mx="auto">
-        <InputGroup mb={4}>
-          <InputLeftElement pointerEvents="none" children={<FaUser color="#657ce0" />} />
-          <Input type="text" placeholder="Sender Name" />
-        </InputGroup>
-<Box mt={5}>
-  <Text my={3} color="red" size="md"> Enter phone numbers and separate it with comma. </Text> 
-  
-</Box>
-        <Textarea onChange={(e)=>setNumbers(e.target.value)} mb={4} placeholder="08147823198,07033445578,09163526373" />
-
-        <Textarea onChange={(e)=> setMessage(e.target.value)} placeholder="Type your message...." />
-
-        <Box mb={4} textAlign="center">
-          
-          <Box mt={2} fontWeight="bold">
-            Amount to be Charged: ₦{cost}
-          </Box>
-        </Box>
-
-        <Button colorScheme="blue" size="lg" width="100%">
-          Send
-        </Button>
-      </Box>
-    </Container>
-  
-    </ChakraProvider>
-      <NavbarBottom/>
-    </>
-      );
-    
-
-};
-
-export default SendBulkSMS;
+            >
+              <Spinner color="#657ce0" size="lg" />
