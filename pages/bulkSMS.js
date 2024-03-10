c31b97bfd2bad38dac9121cff4d81066c13cc6d4
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-10 07:39:16 +0100

    Update bulkSMS.js

--- pages/bulkSMS.js
+++ pages/bulkSMS.js
@@ -91,3 +91,50 @@ useEffect(() => {
 
             >
               <Spinner color="#657ce0" size="lg" />
+              <p></p>
+            </Box>
+          </Center>):(
+
+
+    <Container bg={currentMode==="dark" && "black"} color={currentMode=="dark" && "white"}  maxW="xl" centerContent p={4}>
+      <Box m="auto" textAlign="center">
+        <Heading as="h1" mb={6}>
+          Send Bulk SMS
+        </Heading>
+      </Box>
+
+      <Box width={{ base: '100%', md: '50%' }} mx="auto">
+        <InputGroup mb={4}>
+          <InputLeftElement pointerEvents="none" children={<FaUser color="#657ce0" />} />
+          <Input type="text" placeholder="Sender Name" />
+        </InputGroup>
+<Box mt={5}>
+  <Text my={3} color="red" size="md"> Enter phone numbers and separate it with comma. </Text> 
+  
+</Box>
+        <Textarea onChange={(e)=>setNumbers(e.target.value)} mb={4} placeholder="08147823198,07033445578,09163526373" />
+
+        <Textarea onChange={(e)=> setMessage(e.target.value)} placeholder="Type your message...." />
+
+        <Box mb={4} textAlign="center">
+          
+          <Box mt={2} fontWeight="bold">
+            Amount to be Charged: ₦{cost}
+          </Box>
+        </Box>
+
+        <Button colorScheme="blue" size="lg" width="100%">
+          Send
+        </Button>
+      </Box>
+    </Container>)}
+  
+    </ChakraProvider>
+      <NavbarBottom/>
+    </>
+      );
+    
+
+};
+
+export default SendBulkSMS;
