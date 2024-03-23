1b12e196893fc5078291f50b72315fdfababc325
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-23 12:15:02 +0100

    Update adverts.js

--- components/adverts.js
+++ components/adverts.js
@@ -32,7 +32,7 @@ export default function Adverts() {
           <SwiperSlide key={index}>
             <Image
               w="600px"
-              h="150px"
+              h="50px"
              
               src={image}
               alt={`Image ${index + 1}`}
