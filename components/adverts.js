2e810f8aa2bb3e4730b3c0a3cb03da32f702ce75
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-23 07:07:00 +0100

    Update adverts.js

--- components/adverts.js
+++ components/adverts.js
@@ -7,10 +7,14 @@ import 'swiper/css/effect-cube';
 
 export default function Adverts() {
   const images = [
+    '/advert1.jpg',
+    'advert2.jpg',
+    'advert3.jpg',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToTKK4-ja1SZ-cmxoYmVvKzXMdhA2Gb6CHRg&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaHSgvHNaAzXN5AQzAYR-PgZpoPnJAjwWDWA&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcBfrr3EXfA1KQhBMLImnDWpi2SpUFJENItg&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY1mGdzcBKqEJRgYgxr4O2Rgq14aNiBhkMJQ&usqp=CAU',
+    
   ];
 
   return (
