673a3f649d5cc532f61f4465f329b99a087ca477
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-23 01:57:11 +0100

    Update menu.js

--- components/menu.js
+++ components/menu.js
@@ -107,7 +107,7 @@ export default function Menu() {
             </Box>
           </Center>
         ) : (
-          <Box mt={6}>
+          <Box boxShadow="md" mt={6}>
             <Box maxW="md" mx="2em" p={2}>
               <Grid templateColumns="repeat(4, 1fr)" gap={6}>
               <GridItem colSpan={1} onClick={openBuyData} cursor="pointer">
