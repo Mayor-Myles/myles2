e20797fde9827edc9d27a499d71b148c3c223838
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-23 08:52:52 +0100

    Update adverts.js

--- components/adverts.js
+++ components/adverts.js
@@ -1,32 +1,31 @@
 import { Image } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, EffectCube } from 'swiper/modules';
+import SwiperCore, { Autoplay, EffectFade, Pagination, Navigation } from 'swiper/core';
 import 'swiper/css';
-import 'swiper/css/autoplay';
-import 'swiper/css/effect-cube';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+SwiperCore.use([Autoplay, EffectFade, Pagination, Navigation]);
 
 export default function Adverts() {
   const images = [
     '/advert1.jpg',
-    'advert2.jpg',
-    'advert3.jpg',
+    '/advert2.jpg',
+    '/advert3.jpg',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToTKK4-ja1SZ-cmxoYmVvKzXMdhA2Gb6CHRg&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaHSgvHNaAzXN5AQzAYR-PgZpoPnJAjwWDWA&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcBfrr3EXfA1KQhBMLImnDWpi2SpUFJENItg&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY1mGdzcBKqEJRgYgxr4O2Rgq14aNiBhkMJQ&usqp=CAU',
-    
   ];
 
   return (
     <div className="spaceTop">
       <Swiper
-        modules={[Autoplay, EffectCube]}
         spaceBetween={30}
         slidesPerView={2}
         navigation
         pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        autoplay={1200}
+        autoplay={{ delay: 1200 }}
         effect="slide"
       >
         {images.map((image, index) => (
