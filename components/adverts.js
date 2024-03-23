2af99fcf9b6b2abceb2c25d067fbff20d281de2e
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-23 14:51:15 +0100

    Update adverts.js

--- components/adverts.js
+++ components/adverts.js
@@ -8,14 +8,18 @@ const Adverts = () => {
   const images = [
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0YZlN4HFys6NdcDj1Azbe2lJRgSWs-tRDwQ&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnBRZwcdumfCgnlaCiyPEQfjYRe_XrVkEbLA&usqp=CAU',
-    'image3.jpg',
-    'image4.jpg'
+    'https://st.depositphotos.com/49078592/53424/i/450/depositphotos_534247820-stock-illustration-hand-writing-sign-hire-us.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRTRjjRv9TJx164vNwc2YLBRt8ybMad1rPbw&usqp=CAU',
+    'https://myoperator.com/blog/wp-content/uploads/2023/03/image-1.png',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDGYNA71rY7b6aqoKJ_onWkNbQTqW4IDlBGA&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvyARTaVRhH_29mL1Op2StZ-iZvVwqFAblmA&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBrYh7eZRH0aUjPP9g5b_Z2ntDeVElyiWWLA&usqp=CAU',
   ];
 
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
