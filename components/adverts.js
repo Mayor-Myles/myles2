ee69fcb0aa2138ba312e76d39df8516034b6f508
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-24 01:59:20 +0100

    Update adverts.js

--- components/adverts.js
+++ components/adverts.js
@@ -6,29 +6,29 @@ import { Box, Image } from '@chakra-ui/react';
 
 const Adverts = () => {
   const images = [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0YZlN4HFys6NdcDj1Azbe2lJRgSWs-tRDwQ&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnBRZwcdumfCgnlaCiyPEQfjYRe_XrVkEbLA&usqp=CAU',
-    'https://st.depositphotos.com/49078592/53424/i/450/depositphotos_534247820-stock-illustration-hand-writing-sign-hire-us.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRTRjjRv9TJx164vNwc2YLBRt8ybMad1rPbw&usqp=CAU',
-    'https://myoperator.com/blog/wp-content/uploads/2023/03/image-1.png',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDGYNA71rY7b6aqoKJ_onWkNbQTqW4IDlBGA&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvyARTaVRhH_29mL1Op2StZ-iZvVwqFAblmA&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBrYh7eZRH0aUjPP9g5b_Z2ntDeVElyiWWLA&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwX19JuyuhOmNJat6xrPx-AuakYHf-SbmC_Q&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3tlODApQpYTQnKRMo6sm9jn0I9fsX7atWHA&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNToYlkezrP9Ynd_peA4yVGD5KuNoAXmqmoA&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYXAV6d-i8Gt-q4nrgH0dN8qqrP5hsqgTYtg&usqp=CAU',
+    '',
+    '',
+    '',
+    '',
   ];
 
   const settings = {
     dots: true,
     infinite: true,
     speed: 1500,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000, // adjust as needed
-    customPaging: function(i) {
+    /*customPaging: function(i) {
       return (
         <button style={{color: 'blue'}}>{i + 1}</button>
       );
-    },
+    },*/
   };
 
   return (
@@ -36,7 +36,7 @@ const Adverts = () => {
       <Slider {...settings}>
         {images.map((image, index) => (
           <Box key={index}>
-            <Image w="100%" h="8em" src={image} alt={`Slide ${index}`} />
+            <Image w="100%" h="7em" src={image} alt={`Slide ${index}`} />
           </Box>
         ))}
       </Slider>
