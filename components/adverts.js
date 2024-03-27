b582a5ef5c6a533be459c308034fc10dc867761f
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-27 10:04:28 +0100

    Update adverts.js

--- components/adverts.js
+++ components/adverts.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Image,Flex } from '@chakra-ui/react';
 
 const Adverts = () => {
   const images = [
