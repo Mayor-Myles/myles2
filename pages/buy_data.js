7315d43c06de7b95ea3d7d7aa2cd57e77e456192
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-29 13:39:43 +0100

    Update buy_data.js

--- pages/buy_data.js
+++ pages/buy_data.js
@@ -258,7 +258,7 @@ const dataPlansDetail = [
                 ) : (
          <Container bg={currentMode === "dark" && "black"} color={currentMode ==="dark"&&"white"} h="100vh"  maxW="100vw" m={0}>       
         <Box alignItems="center" justifyContent="center" flexDirection="column">
-          <Box p={{ base: 5, sm: "2em", md: "3em", lg: "4em", xl: "5em" }} h="80vh" boxShadow="md" textAlign="center">
+          <Box p={{ base: "2em", sm: "2em", md: "3em", lg: "4em", xl: "5em" }} h="80vh" boxShadow="md" textAlign="center">
  <Heading as="h1" size="md" fontFamily="sans-serif" mb="4">
               Buy Data
             </Heading>
