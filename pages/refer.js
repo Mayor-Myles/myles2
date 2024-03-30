cd42210de40906c71253d228fcd3d395335b8cff
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-03-30 09:01:53 +0100

    Update refer.js

--- pages/refer.js
+++ pages/refer.js
@@ -9,7 +9,8 @@ import {
   Heading,
   IconButton,
   Text,
-  useColorMode,Spinner
+  useColorMode,
+  Spinner
 } from '@chakra-ui/react';
 import { FiShare2, FiSun, FiMoon } from 'react-icons/fi';
 import { useRecoilValue, useSetRecoilState } from "recoil";
@@ -24,8 +25,8 @@ const Refer = () => {
   const setLoadingPage = useSetRecoilState(pageLoading);
   const currentMode = useRecoilValue(mode);
   const setMode = useSetRecoilState(mode);
-const currentPage = useRecoilValue(thisPage);
-const setCurrentPage = useSetRecoilState(thisPage);
+  const currentPage = useRecoilValue(thisPage);
+  const setCurrentPage = useSetRecoilState(thisPage);
 
   useEffect(() => {
     if (!currentMode) {
@@ -34,20 +35,22 @@ const setCurrentPage = useSetRecoilState(thisPage);
   }, [currentMode, setMode]);
 
   useEffect(() => {
-    const userChoice = localStorage.getItem("mode"); // Access localStorage directly without window object
+    const userChoice = localStorage.getItem("mode");
 
     if (userChoice === "dark" || userChoice === "light") {
-     const userChoice = localStorage.setItem("mode",userChoice); // Access localStorage directly without window object
-
+      localStorage.setItem("mode", userChoice); // Access localStorage directly without window object
       setMode(userChoice);
+    } else {
+      localStorage.setItem("mode", "light"); // Set default mode if no mode is found in localStorage
+      setMode("light");
     }
-  }, [mode,setMode]);
+  }, [currentMode, setMode]);
 
-  useEffect(()=>{
+  useEffect(() => {
     setCurrentPage("refer");
     setLoadingPage(false);
-  },[]);
-  
+  }, []);
+
   const handleCopy = () => {
     navigator.clipboard.writeText('Your referral link here');
     setCopied(true);
@@ -55,68 +58,71 @@ const setCurrentPage = useSetRecoilState(thisPage);
       setCopied(false);
     }, 2000);
   };
-if(!currentMode){
-return null;
-}
+
+  if (!currentMode) {
+    return null;
+  }
+
   return (
     <>
-    <ChakraProvider>
-  
-      <Header /> {/* Include Header component */}
-  {loadingPage ? (
-    <Container h="100vh" maxW="100vw" bg={currentMode==="dark" && "black"}>
-<Box display="flex" justify="center" align="center">
-<Spinner size="xl" color="blue" />
-    </Box></Container>
-    ) :(
-        <Container bg={currentMode == "dark" && "black" } h="100vh" maxW="100vw">
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Box maxW="550px" h="80vh" p={8} borderWidth="0px" borderRadius="lg" boxShadow="sm" bg={currentMode === 'light' ? 'white' : 'black'}>
-              <Heading mb={4} textAlign="center" color="dodgerblue">
-                Refer & Earn!
-              </Heading>
-              <Text color={currentMode=="dark" && "white"} textAlign="center" mb={4}>
-                Bring us a client for <b>Graphics Design</b> or <b>Website development</b> project and get paid immediately your refferal pays us!!! There is no limit to the number of clients you can bring. The more you bring the more you earn. All you need to do is to tell your refferal to use our <b>"Hire Me"</b> menu and then input your referral code. Contact us for more information... 
-              </Text>
-              <Flex justifyContent="center" alignItems="center" mb={4}>
-                <Box flex="1">
-                  <Text textAlign="center" fontWeight="bold" color="gray.600">
-                    Your Referral Code
-                  </Text>
-                  <Text textAlign="center" mt={2} color="gray.500">
-                    AXDQNY23
-                  </Text>
-                </Box>
-                <IconButton
-                  icon={<FiShare2 />}
-                  aria-label="Copy Referral Link"
-                  ml={2}
-                  onClick={handleCopy}
-                  colorScheme="blue"
-                  variant="ghost"
-                />
-              </Flex>
-              {copied && (
-                <Text textAlign="center" color="green.500" fontSize="sm">
-                  Copied to clipboard!
-                </Text>
-              )}
-              <Button
-                mt={4}
-                bg="dodgerblue"
-                color="white"
-                variant="solid"
-                w="full"
-                onClick={handleCopy}
-              >
-                Copy Referral code
-              </Button>
+      <ChakraProvider>
+        <Header /> {/* Include Header component */}
+        {loadingPage ? (
+          <Container h="100vh" maxW="100vw" bg={currentMode === "dark" && "black"}>
+            <Box display="flex" justify="center" align="center">
+              <Spinner size="xl" color="blue" />
             </Box>
-    </Box>
-        </Container>)}
-      
-           </ChakraProvider>
-<NavbarBottom />
+          </Container>
+        ) : (
+          <Container bg={currentMode == "dark" && "black"} h="100vh" maxW="100vw">
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <Box maxW="550px" h="80vh" p={8} borderWidth="0px" borderRadius="lg" boxShadow="sm" bg={currentMode === 'light' ? 'white' : 'black'}>
+                <Heading mb={4} textAlign="center" color="dodgerblue">
+                  Refer & Earn!
+                </Heading>
+                <Text color={currentMode == "dark" && "white"} textAlign="center" mb={4}>
+                  Bring us a client for <b>Graphics Design</b> or <b>Website development</b> project and get paid immediately your referral pays us!!! There is no limit to the number of clients you can bring. The more you bring the more you earn. All you need to do is to tell your referral to use our <b>"Hire Me"</b> menu and then input your referral code. Contact us for more information...
+                </Text>
+                <Flex justifyContent="center" alignItems="center" mb={4}>
+                  <Box flex="1">
+                    <Text textAlign="center" fontWeight="bold" color="gray.600">
+                      Your Referral Code
+                    </Text>
+                    <Text textAlign="center" mt={2} color="gray.500">
+                      AXDQNY23
+                    </Text>
+                  </Box>
+                  <IconButton
+                    icon={<FiShare2 />}
+                    aria-label="Copy Referral Link"
+                    ml={2}
+                    onClick={handleCopy}
+                    colorScheme="blue"
+                    variant="ghost"
+                  />
+                </Flex>
+                {copied && (
+                  <Text textAlign="center" color="green.500" fontSize="sm">
+                    Copied to clipboard!
+                  </Text>
+                )}
+                <Button
+                  mt={4}
+                  bg="dodgerblue"
+                  color="white"
+                  variant="solid"
+                  w="full"
+                  onClick={handleCopy}
+                >
+                  Copy Referral code
+                </Button>
+              </Box>
+            </Box>
+          </Container>
+        )}
+
+      </ChakraProvider>
+      <NavbarBottom />
     </>
   );
 };
