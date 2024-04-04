d7a16f68e6fbee2111d3f527d78dcbaff4182290
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-04-04 09:08:11 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,8 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   
   useEffect(() => {
     const currentMode = window.localStorage.getItem("mode");
-  alert("app mode is "+ currentMode);
-    
+  
     //User hasn't chosen preffered mode
     if (currentMode !== "dark") {  
       window.localStorage.setItem("mode", "light");
